472963d9e67ba24ac628566badf318853251a6e8
Dimitri Ratz <dimitri.ratz@pm.me>
2022-10-24 00:10:27 +0200

    Print lib version.

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,14 @@
 #include <hello_world_cpp/library.hpp>
+#include <hello_world_cpp/version.hpp>
 
 #include <iostream>
 
 int main()
 {
     std::cout << "Hello World app started." << std::endl;
-    const td::HelloWorld api_object {};
+    const hello_world::HelloWorld api_object {};
     api_object.print_hello();
+    std::cout << "Hello World lib GIT revision: " << hello_world::Version::get_git_hash() << std::endl;
 
     return 0;
 }
