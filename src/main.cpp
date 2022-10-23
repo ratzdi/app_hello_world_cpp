dc6f2924d261ac84655a126e28ace0d590148faf
Dimitri Ratz <dimitri.ratz@pm.me>
2022-10-23 23:12:15 +0200

    Fixed dependency setup. Minor changes.

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,10 @@
 
 #include <iostream>
 
-int main() {
-    std::cout << "Hello World app started" << std::endl;
-    const td::HelloWorld api_object{};
+int main()
+{
+    std::cout << "Hello World app started." << std::endl;
+    const td::HelloWorld api_object {};
     api_object.print_hello();
 
     return 0;
